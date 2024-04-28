98119f6d26db8cc77334122310fdf81ea9fd6f99
WispySkies <arpolixgames@gmail.com>
2024-04-28 15:35:17 -0400

    first attempt

--- code_solution/min_graph_coloring.py
+++ code_solution/min_graph_coloring.py
@@ -5,51 +5,72 @@ Decision-based Minimum Graph Coloring
 
 Declan McCue - Conor McFadden
 
-
-some notes:
-reducing problem to can you color a complete graph with K colors
-computationally NP-Hard
-
-runtimes on complete graphs:
-fixed K, decision on colors >=K, linear
-Optimization: O( |V| / sqrt (log|V|) )
-
-Transformations/reductions from minimal maximum matching (Yannakakis and Gavril, 1978)
-might be worth a read: https://www.jstor.org/stable/2100648
-
+Some helpful references:
 
 https://en.wikipedia.org/wiki/Graph_coloring
 https://en.wikipedia.org/wiki/Complete_coloring
-
-
-Presentation problem: Graph coloring is NP-Hard, must reduce to a NP-Complete
-Reduce graph coloring to min >= K?
-
-Find: optimal solution of NP-Complete, then find an approximation and do an analysis (must be polynomial)
 """
 
 from collections import defaultdict
 
+def color(G, v) -> list[tuple]:
+    
+    # can color vertex with color
+    def is_safe_assignment(vertex, colors, proposed) -> bool:
+        for neighbor in G[vertex]:
+            if colors[neighbor] == proposed:
+                return False
+        return True
+
+    # backtracking recursive coloring
+    def color_graph(vertex, colors) -> bool:
+        print(colors)
+        if vertex == len(G):
+            return True
+        
+        for color in range(1, v + 1):
+            if is_safe_assignment(vertex, colors, color):
+                colors[vertex] = color
+                if color_graph(vertex + 1, colors):
+                    return True
+                colors[vertex] = color
+        
+        return False
+
+    # iterate over 
+    colors = [0] * (len(G) + 1)
+    if color_graph(0, colors):
+        return [(vertex, colors[vertex]) for vertex in range(len(G))]
+    else:
+        return None
+
+def min_graph_coloring(G, V) -> int:
+    colors = V + 1
+    assignments = []
+    for i in range(V + 1, -1, -1):
+        new_assignments = color(G, i)
+        if new_assignments:
+            colors = min(colors, i)
+            assignments = new_assignments
+    return colors, assignments
 
 def main():
     K = int(input())
     pairs = [input().split() for _ in range(K)]
 
-    # build color assignments by vertex : color
-    assignments = {v: int(k) for v, k in pairs}
-
     # build adj-list based graph on assignments
-    # must it be complete? i.e. all vertices connect
-    graph = defaultdict(list)
-    for v1, _ in assignments.items():
-        for v2, _ in assignments.items():
-            # skip edges to self 
-            if v1 == v2:
-                continue
+    graph = defaultdict(set)
+    for v1, v2 in pairs:
+        graph[v1].add(v2)
+        graph[v2].add(v1)
+    
+    # min colors is realistically between 0 and V
+    # could argue lower, count vertices and iterate
+    V = len(set().union(*graph.values()))
 
-            graph[v1].append(v2)
-            graph[v2].append(v1)
-
+    min_colors, assignments = min_graph_coloring(graph, V)
+    print(min_colors)
+    print(assignments)
 
 if __name__ == "__main__":
     main()
